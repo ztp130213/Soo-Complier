2fdaa8f15dcc2643246f14573c7058292fd0a7a5
孙海清 <Leviathan1995@outlook.com>
2014-12-30 14:35:41 +0800

    My_FirstToyComplier
    src

---
+++ src/Parser.cpp
@@ -0,0 +1,561 @@
+#include "ParserHead.h"
+#include "LexerHead.h"
+#include <stack>
+#include <string>
+using namespace std;
+Lexer Lexer_out;//词法分析的结果
+/*
+变量
+*/
+//变量查找，已知存在，查找具体变量
+Variable_tag *Search(string text, Variable_tag *First)
+{
+	Variable_tag *test = First->next;
+	while (test!= NULL)
+	{
+		if (test->name == text)
+			return test;
+	}
+}
+//判断是不是变量
+bool isVariable(string Text, Variable_tag *First)
+{
+	Variable_tag *test = First;
+	while (test!= NULL)
+	{
+		if (test->name == Text)
+			return true;
+		test = test->next;
+	}
+	return false;
+}
+//得出当前词法字符流的值,已确定是变量
+int Get_Variable_number(Variable_tag *First)
+{
+	int number;
+	if (Lexer_out.peek(0).isnumber())
+	{
+		number = Lexer_out.read().getnumber();
+	}
+	else if (isVariable(Lexer_out.peek(0).gettext(), First))
+	{
+		number;
+		Variable_tag *test = Search(Lexer_out.peek(0).gettext(), First);
+		number = test->value.u.int_vlaue;
+	}
+	Lexer_out.read();
+	return number;
+}
+
+//变量不存在链表中，即添加
+void Add_Variable(Variable_tag * &First)
+{
+	string text;
+	Variable_tag *add = First;
+	while (add->next != NULL)
+	{
+		add = add->next;
+	}
+	Variable_tag *New = new Variable_tag;
+	text = Lexer_out.read().gettext();
+	New->name = text;
+	if (Lexer_out.read().gettext() == "=")
+	{
+		if (isVariable(Lexer_out.peek(0).gettext(), First))
+			New->value = Search(Lexer_out.peek(0).gettext(), First)->value;
+		else if (Lexer_out.peek(0).isnumber())
+		{
+			New->value.u.int_vlaue = Lexer_out.read().getnumber();
+			New->value.type = Int_value;
+		}
+	}
+	New->next = NULL;
+	add->next = New;
+}
+//判断表达中的是变量还是数值并返回
+int Judeg_Variable(Variable_tag *First)
+{
+	if (isVariable(Lexer_out.peek(0).gettext(), First))
+	{
+		int number = 0;
+		number = Search(Lexer_out.peek(0).gettext(), First)->value.u.int_vlaue;
+		return number;
+	}
+	else//(Lexer_out.peek(0).getnumber())
+	{
+		int number = Lexer_out.read().getnumber();
+		return number;
+	}
+}
+/*
+		算术表达式
+*/
+//算术表达式的语法树
+int i, sum = 0;
+char *str = 0;
+//判断一个字符是否为数字
+bool isdigit(char c)
+{
+	if (c - '0' <= 9 && c - '0' >= 0)
+		return true;
+	else
+		return false;
+}
+Binary_Tree * parse_F(Binary_Tree *&sun);
+Binary_Tree * parse_T(Binary_Tree *&sun);
+Binary_Tree * parse_E(Binary_Tree *&root);
+//因子
+Binary_Tree * parse_F(Binary_Tree *&sun)
+{
+	Binary_Tree *L = new Binary_Tree;
+	char c = str[i];
+	if (isdigit(c)){
+		i++;
+		L->data = c;
+		L->left = NULL;
+		L->right = NULL;
+		return L;
+	}
+	if (c == '(')
+	{
+		i++;
+		L = parse_E(sun);
+		c = str[i];
+		if (c == ')')
+		{
+			i++;
+			return L;
+		}
+		return L;
+	}
+	return L;
+}
+//项
+Binary_Tree * parse_T(Binary_Tree *&sun)
+{
+	Binary_Tree *L = new Binary_Tree;
+	L->data = 'T';
+	L->left = parse_F(sun);
+	L->right = NULL;
+	char c = str[i];
+	while (c == '*' || c == '/'){
+		i++;
+		if (L->right == NULL)
+		{
+			L->right = parse_F(sun);
+			L->data = c;
+		}
+		else
+		{
+			Binary_Tree * P = new Binary_Tree;
+			P->right = parse_F(P);
+			P->left = L;
+			P->data = c;
+			L = P;
+		}
+		c = str[i];
+	}
+	return L;
+}
+//表达式
+Binary_Tree * parse_E(Binary_Tree *&root)
+
+{
+	Binary_Tree *sun = new Binary_Tree;
+	sun->data = 'E';
+	sun->left = parse_T(sun);
+	sun->right = NULL;
+	char c = str[i];
+	while (c == '+' || c == '-'){
+		i++;
+		if (sun->right == NULL)
+		{
+			sun->right = parse_T(sun);
+			sun->data = c;
+		}
+		else
+		{
+			Binary_Tree *P = new Binary_Tree;
+			P->right = parse_E(P);
+			P->left = sun;
+			P->data = c;
+			sun = P;
+		}
+		c = str[i];
+	}
+	if (c == '\0')
+		return sun;
+	return sun;
+}
+//后序遍历算术表达式的树
+void post(Binary_Tree * &root, stack<int> s)
+{
+	int a, b;
+	if (root != NULL)
+	{
+		post(root->left, s);
+		post(root->right, s);
+		if (s.size() >= 2)
+		{
+			if (root->data == '+' || root->data == '-' || root->data == '/' || root->data == '*')
+			{
+				a = s.top() - '0';
+				s.pop();
+				b = s.top() - '0';
+				s.pop();
+				if (root->data == '+')
+					a = a + b;
+				if (root->data == '-')
+					a = b - a;
+				if (root->data == '*')
+					a = a * b;
+				if (root->data == '/')
+					a = b / a;
+				a = a + 48;
+				s.push(a);
+			}
+			else
+			{
+				if (isdigit(root->data) || root->data == '+' || root->data == '-' || root->data == '/' || root->data == '*')
+					s.push(root->data);
+			}
+		}
+		else
+		{
+			if (isdigit(root->data) || root->data == '+' || root->data == '-' || root->data == '/' || root->data == '*')
+				s.push(root->data);
+		}
+	}
+}
+//返回算术表达式语法分析的结果
+int BinaryParse()
+{
+	stack<int> s; //计算算术表达式的栈
+	i = 0;
+	Binary_Tree * L = new Binary_Tree;
+	Binary_Tree *sun = new Binary_Tree;
+	L = parse_E(sun);
+	return s.top();
+}
+/*
+		条件判断表达式
+*/
+/*
+判断表达式的语法分析树是否为真
+根据符号的优先级类比算数运算符进去比较
+*/
+//判断表达式中<,>,<=,>=1级运算符
+Tree_Judge* G_L_Expreesion(Variable_tag *First)
+{
+	Tree_Judge *L = new Tree_Judge;
+	int number = Judeg_Variable(First);
+	L->left = new Tree_Judge;
+	L->right = new Tree_Judge;
+	L->left->u.data = number;
+	Lexer_out.read();
+	while (Lexer_out.peek(0).gettext() == "<" || Lexer_out.peek(0).gettext() == ">" || Lexer_out.peek(0).gettext() == "<=" || Lexer_out.peek(0).gettext() == ">=" || Lexer_out.peek(0).gettext() == "!=")
+	{
+		char *c;
+		const int len = Lexer_out.peek(0).gettext().length();
+		c= new char[len + 1];
+		strcpy(c, Lexer_out.read().gettext().c_str());
+		L->u.sign = c;
+		number=Judeg_Variable(First);
+		L->right->u.data = number;
+	}
+	L->right->right = NULL;
+	L->right->left = NULL;
+	L->left->right = NULL;
+	L->left->left = NULL;
+	return L;
+
+}
+//判断表达式中&&2级运算符
+Tree_Judge * AND_Expreesion(Variable_tag *First)
+{
+	Tree_Judge *L = new Tree_Judge;
+	L= G_L_Expreesion(First);
+	L= NULL;
+	while (Lexer_out.peek(0).gettext() == "&&")
+	{
+		char *c;
+		const int len = Lexer_out.peek(0).gettext().length();
+		c = new char[len + 1];
+		strcpy(c, Lexer_out.read().gettext().c_str());
+		L->u.sign = c;
+		Lexer_out.read();
+		L->right = G_L_Expreesion(First);
+	}
+	return L;
+}
+//判断表达式中||3级运算符
+Tree_Judge * OR_Expreesion(Variable_tag *First)
+{
+	Tree_Judge *out = new Tree_Judge;
+	out= AND_Expreesion(First);
+	out->right= NULL;
+	out->left = NULL;
+	while (Lexer_out.peek(0).gettext() == "||")
+	{
+		char *c;
+		const int len = Lexer_out.peek(0).gettext().length();
+		c = new char[len + 1];
+		strcpy(c, Lexer_out.read().gettext().c_str());
+		out->u.sign = c;
+		out->right = AND_Expreesion(First);
+	}
+	return out;
+}
+//后序遍历判断表达式所需要的栈
+stack<Tree_Judge *> Expreesion_Stack;
+//后序遍历判断表达式的语法树
+void Postinorder_Expreesion(Tree_Judge * &Judeg_Expreesion)
+{
+	if (Judeg_Expreesion != NULL)
+	{
+		Postinorder_Expreesion(Judeg_Expreesion->left);
+		Postinorder_Expreesion(Judeg_Expreesion->right);
+		if (Expreesion_Stack.size() >= 2)
+		{
+			string s(Judeg_Expreesion->u.sign);
+			if (s=="<") //在栈中判断<
+			{
+				int one, two;
+				one = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				two = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				if (two < one)
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 1;
+					Expreesion_Stack.push(statue);
+				}
+				else
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 0;
+					Expreesion_Stack.push(statue);
+				}
+			}
+			if (Judeg_Expreesion->u.sign == ">") //在栈中判断>
+			{
+				int one, two;
+				one = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				two = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				if (one > two)
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 1;
+					Expreesion_Stack.push(statue);
+				}
+				else
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 0;
+					Expreesion_Stack.push(statue);
+				}
+			}
+			if (Judeg_Expreesion->u.sign == "<=")
+			{
+				int one, two;
+				one = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				two = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				if (one <= two)
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 1;
+					Expreesion_Stack.push(statue);
+				}
+				else
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 0;
+					Expreesion_Stack.push(statue);
+				}
+			}
+			if (Judeg_Expreesion->u.sign == ">=")
+			{
+				int one, two;
+				one = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				two = Expreesion_Stack.top()->u.data;
+				Expreesion_Stack.pop();
+				if (one >= two)
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 1;
+					Expreesion_Stack.push(statue);
+				}
+				else
+				{
+					Tree_Judge *statue = new Tree_Judge;
+					statue->type = Bool;
+					statue->u.bool_output = 0;
+					Expreesion_Stack.push(statue);
+				}
+			}
+		}
+		else
+			Expreesion_Stack.push(Judeg_Expreesion);
+	}
+}
+//判断表达式的BOOL值
+bool  Bool_Expreesion(Variable_tag *First)
+{
+	bool Judge_Ex;
+	Tree_Judge * Judge_Expression = new Tree_Judge;
+	Judge_Expression=OR_Expreesion(First);
+	Postinorder_Expreesion(Judge_Expression);
+	Judge_Ex = Judge_Expression->u.bool_output;
+	return Judge_Ex;
+}
+/*
+	表达式的具体函数
+*/
+void Assign_Expression(Variable_tag *First)
+{
+	if (Search(Lexer_out.peek(0).gettext(),First)->value.type == Int_value)
+	{
+		Assign_Tree *Assign = new Assign_Tree;
+		Assign->left = Search(Lexer_out.read().gettext(),First)->value;
+		Binary_Tree *Binary = new Binary_Tree;
+		Assign->right = parse_E(Binary);
+		Assign->left.u.int_vlaue = BinaryParse();
+	}
+}
+/*
+	语句块
+*/
+//执行语句块
+void Block_run(Variable_tag *First)
+{
+	Block * StatementList = new Block;
+	if (Lexer_out.peek(0).gettext() == "{")
+	{
+		Lexer_out.read();
+		//是赋值表达式
+		if (Lexer_out.peek(1).gettext() == "=")
+		{
+			//算术赋值
+			if (isVariable(Lexer_out.peek(0).gettext(),First))
+			{
+				Assign_Expression(First);
+			}
+		}
+	}
+}
+/*
+	关键字
+*/
+//Print关键字，即输出变量值
+void Printvalue(Variable_tag *example)
+{
+	switch (example->value.type)
+	{
+	case Int_value: //变量是int型
+	{
+						int number;
+						number = example->value.u.int_vlaue;
+						cout << number;
+	}
+	default:
+		break;
+	}
+}
+//判断是不是关键字
+bool IsKeyword(string s)
+{
+	if (s == "while")
+		return true;
+	else if (s == "print")
+		return true;
+	else
+		return false;
+}
+//是什么关键字
+void IswhatKeyword(string text, Variable_tag *First)
+{
+	Statement_tag goal;
+	string test;
+	if (text == "while") //关键字是While
+	{
+		Lexer_out.read();
+		goal.type = WHILE_STATEMENT;
+		test= Lexer_out.peek(0).gettext();
+		if(test[1]=='(')
+		{
+			Lexer_out.read();
+			//转到判断表达式的真值
+			for (;;)
+			{
+				if (Bool_Expreesion(First))
+					break;
+				else
+					Block_run(First);//否则执行语句块
+			}
+		}
+	}
+	else if (text == "print") //关键字是Print
+	{
+		string test = Lexer_out.peek(0).gettext();
+		if (isVariable(test,First))
+		{
+			Variable_tag *example = Search(text,First);
+			Printvalue(example);
+		}
+	}
+}
+//语法分析
+void Parser()
+{
+	Variable_tag *First = new Variable_tag;//全局变量链表的头指针
+	First->next= NULL;
+	string text;
+	while (Queue.size() != 0)
+	{
+		text = Lexer_out.peek(0).gettext();
+		if (text == "\\n")
+		{
+			Lexer_out.read();
+			text = text = Lexer_out.peek(0).gettext();
+		}
+		if (IsKeyword(text))//是关键字
+			IswhatKeyword(text,First);
+		else if (Lexer_out.peek(1).gettext() == "=") //赋值语句
+		{
+			Add_Variable(First);
+		}
+		else if (isVariable(text,First)) //是变量
+		{
+
+		}
+		
+	}
+}
+int main()
+{
+	int linenumber = 0;
+	Token t;
+	ifstream infile("string.txt");
+	string line;
+	while (getline(infile, line))
+	{
+
+		linenumber++;
+		Lexer_out.readline(line, linenumber);
+	}
+	Parser();
+	return 0;
+}
---
+++ src/ParserHead.h
@@ -0,0 +1,198 @@
+/*
+语法分析
+*/
+#ifndef PARSERHEAD_H_
+#define PARSERHEAD_H_
+#include "LexerHead.h"
+#include <string>
+using namespace std;
+/*
+变量
+*/
+//变量类型
+typedef enum
+{
+	Boolean_value = 1,
+	Int_value,
+	Double_value,
+	String_value,
+	NULL_value
+}Value_type;
+//变量的结构体
+struct Variable_
+{
+	Value_type type; //变量的类型
+	union
+	{
+		bool boolean_value;
+		int int_vlaue;
+		double double_value;
+		string *string_value; //字符串值
+	}u;
+};
+//全局变量链表
+struct Variable_tag
+{
+	string name; //变量的名字
+	Variable_ value;//变量的值
+	Variable_tag *next;//下一个变量
+};
+/*
+表达式
+*/
+//所有的表达式类型的枚举
+typedef enum {
+	BOOLEAN_EXPRESSION = 1, //bool型常量
+	INT_EXPRESSION,   //int型常量
+	DOUBLE_EXPRESSION,//double型常量
+	STRING_EXPRESSION,//字符串型常量
+	IDENTIFIER_EXPRESSION,//变量
+	ASSIGN_EXPRESSION,//赋值表达式
+	ADD_EXPRESSION,//相加表达式
+	SUB_EXPRESSION,//减法表达式
+	MUL_EXPRESSION,//乘法表达式
+	DIV_EXPRESSION,//除法表达式
+	MOD_EXPRESSION,//求余表达式
+	EQ_EXPRESSION,//==
+	NE_EXPRESSION,//!=
+	GT_EXPRESSION,//>
+	GE_EXPRESSION,//>=
+	LT_EXPRESSION,//<
+	LE_EXPRESSION,//<=
+	LOGICAL_AND_EXPRESSION,//&&
+	LOGICAL_OR_EXPRESSION,//||
+	FUNCTION_CALL_EXPRESSION,//函数调用表达式
+	MINUS_EXPRESSION,
+	NULL_EXPRESSION,
+	EXPRESSION_TYPE_COUNT_PLUS_1
+} ExpressionType;
+//语句表达式树的结点的类型枚举
+typedef enum
+{
+	Variable = 1, //节点是变量
+	Number,//节点是数字
+	Sign,//节点是符号
+}Statementtype;
+//语句表达式的树形结构
+struct  StatementTree
+{
+	Statementtype type;
+	union
+	{
+		int number;
+		Variable_ Variable;
+		char *sign;
+	}u;
+	StatementTree *left;
+	StatementTree *right;
+};
+//语句结构体
+struct Statementlink
+{
+	ExpressionType type;//语句类型
+	StatementTree * Expreesion_statement;//语句分析结果树
+};
+//语句链表
+struct StatementList {
+	Statementlink  *statement;
+	struct StatementList    *next;
+};
+//算术表达式的树结构
+struct Binary_Tree
+{
+	char data;
+	Binary_Tree *left;
+	Binary_Tree *right;
+};
+/*
+判断条件表达式
+*/
+//判断条件表达式语句结构体
+struct Expression_tag {
+	ExpressionType type;
+	int line_number;
+	union {
+		bool             boolean_value;
+		int                     int_value;
+		double                  double_value;
+		char                    *string_value;
+		//ASSIGN_EXPRESSION      assign_expression;
+		/*
+		char                    *identifier;
+		BinaryExpression        binary_expression;
+		Expression              *minus_expression;
+		FunctionCallExpression  function_call_expression;
+		*/
+	} u;
+};
+//判断表达式的语法分析树的结点类型
+typedef enum
+{
+	charSign = 1,
+	judge_Number,
+	Bool
+}Tree_Judge_Type;
+//构建判断表达式的语法分析树
+struct Tree_Judge
+{
+	Tree_Judge_Type type;
+	union
+	{
+		char* sign;		//节点是运算符
+		int data;	//节点是数值
+		bool bool_output;//左右子树的比较结果
+	}u;
+	Tree_Judge *left;
+	Tree_Judge *right;
+};
+//赋值表达式的树形结构
+struct Assign_Tree
+{
+	Binary_Tree * right;
+	Variable_ left;
+};
+/*
+语句块
+*/
+//语句块结构体
+struct Block
+{
+	StatementList *statementlist;
+
+	union
+	{
+		//ASSIGN_EXPRESSION	Assign_expression;//赋值表达式
+		//BOOLEAN_EXPRESSION	Bool_expression; //bool型常量
+
+		//ADD_EXPRESSION		Add_expreesion; //相加表达式
+		//SUB_EXPRESSION	Sub_expreesion;//减法表达式
+		//MUL_EXPRESSION	Mul_expreesion;	//乘法表达式
+		//DIV_EXPRESSION	Div_expreesion;//除法表达式
+		//MOD_EXPRESSION  Mod_expreesion;//求余表达式
+		//FUNCTION_CALL_EXPRESSION,//函数调用表达式
+	}u;
+};
+/*
+关键字
+*/
+//关键字语句类型
+typedef enum
+{
+	WHILE_STATEMENT
+}StatementType;
+//while语句的结构
+struct WhileStatement
+{
+	Expression_tag *condition;//条件表达式
+	Block* block; //可执行块
+};
+//关键字语句结构体
+struct Statement_tag
+{
+	StatementType type;
+	union
+	{
+		WhileStatement while_s;
+	}u;
+};
+#endif
